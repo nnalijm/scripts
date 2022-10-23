0f73f6bf900ff90e61242fb4f8eee90be8243b20
nnalijm <naaaaaaaaaaaad@gmail.com>
2022-10-23 11:50:21 +0200

    add descriptions

--- grep/grep.sh
+++ grep/grep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+##This simple script shows matches among numbers by using grep##
+##For start script you input file's name where grep should find matches##
+
 FILE=$1
 grep -E '(+48)?((-|\s)?[0-9]){9}' $FILE
